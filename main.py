a296272ef7f49aa9c6eb1fece141464b162b7732
sandraaliyas <54934476+sandraaliyas@users.noreply.github.com>
2021-12-07 16:47:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return "Welcome to HOME PAGE"
 
 
 if __name__ == "__main__":
